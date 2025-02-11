52d4dbc388d35a928debe950a11268d2bdc96efa
yazdanisad <yazdani.msadegh@gmail.com>
2025-02-11 10:37:12 -0500

    Update PortfolioProject-DATACovidallCountries.sql

--- PortfolioProject-DATACovidallCountries.sql
+++ PortfolioProject-DATACovidallCountries.sql
@@ -3,8 +3,8 @@
 
 --select the Data that we are going to using 
 
-select * 
-from portfolioProject.dbo.coviddeaths
+Select * 
+From portfolioProject.dbo.coviddeaths
 where continent is not null
 order by 3,4
 --select * 
@@ -14,14 +14,14 @@ order by 3,4
 
 --Looking at Total cases vs Total deaths:
 
-select country,date,total_cases,new_cases,total_deaths,population 
-from portfolioProject.dbo.coviddeaths
+Select country,date,total_cases,new_cases,total_deaths,population 
+From portfolioProject.dbo.coviddeaths
 order by 1,2
 
 --Show the percentage of death if you contact with covid in your country:
 
-select country,date,total_cases,total_deaths,(total_deaths/total_cases)*100 as DeathPercentage 
-from portfolioProject.dbo.coviddeaths
+Select country,date,total_cases,total_deaths,(total_deaths/total_cases)*100 as DeathPercentage 
+From portfolioProject.dbo.coviddeaths
 where country like '%cana%' 
 order by 1,2
 
@@ -30,16 +30,16 @@ order by 1,2
 --Looking at total cases vs population
 -- The percentage of population that got Covid
 
-select country,date,total_cases,population,(total_cases/population)*100 as PercentPopulationinfected
-from portfolioProject.dbo.coviddeaths
+Select country,date,total_cases,population,(total_cases/population)*100 as PercentPopulationinfected
+From portfolioProject.dbo.coviddeaths
 where country like '%cana%'
 and continent is not null
 order by 2
 
 --Looking ast the countries have highest infection rate vs population  
 
-select country,Max(Cast(total_cases as int)) as HighestInfectionCount ,Max(total_cases/population)*100 as PercentPopulationinfected 
-from portfolioProject.dbo.coviddeaths
+Select country,Max(Cast(total_cases as int)) as HighestInfectionCount ,Max(total_cases/population)*100 as PercentPopulationinfected 
+From portfolioProject.dbo.coviddeaths
 --where country like '%cana%' 
 group by country 
 order by PercentPopulationinfected DESC
@@ -47,8 +47,8 @@ order by PercentPopulationinfected DESC
 
 --Show the countries with highest deaths per population:
 
-select country,Max(Cast(total_deaths as int)) as TotaldeathCount ,Max(cast(total_deaths as int)/population)*100 as PercentPopulationDeath 
-from portfolioProject.dbo.coviddeaths
+Select country,Max(Cast(total_deaths as int)) as TotaldeathCount ,Max(cast(total_deaths as int)/population)*100 as PercentPopulationDeath 
+From portfolioProject.dbo.coviddeaths
 --where country like '%cana%'
 where continent is not null
 group by country 
@@ -56,8 +56,8 @@ order by TotaldeathCount DESC
 
 --break  Data down by continent
 
-select continent,Max(Cast(total_deaths as int)) as TotaldeathCount 
-from portfolioProject.dbo.coviddeaths
+Select continent,Max(Cast(total_deaths as int)) as TotaldeathCount 
+From portfolioProject.dbo.coviddeaths
 --where country like '%cana%'
 where continent is not null
 group by continent 
@@ -66,20 +66,20 @@ order by TotaldeathCount DESC
 
 --Global numbers per every day  and solution for divide by zero condition.
 
-select date,sum(new_cases)as TotalCases,sum(new_deaths) as TotalDeaths,
-(select case
+Select date,sum(new_cases)as TotalCases,sum(new_deaths) as TotalDeaths,
+(Select Case
 			when sum(new_cases) = 0
 			then 0
 			else sum(new_deaths)/sum(cast(new_cases as int))*100
  End) as DeathPercentage 
-from portfolioProject.dbo.coviddeaths
+From portfolioProject.dbo.coviddeaths
 where continent is not null
 group by date
 order by 1
 
 --Total deaths and infected Covid.
 
-select sum(new_cases)as TotalCases,sum(new_deaths) as TotalDeaths,
+Select sum(new_cases)as TotalCases,sum(new_deaths) as TotalDeaths,
 (select case
 			when sum(new_cases) = 0
 			then 0
